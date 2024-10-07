7f29a0b533fa268e3693c8f347902e24ce23fdb9
midnight2606 <161714635+midnight2606@users.noreply.github.com>
2024-10-07 08:53:35 -0300

    Add files via upload

---
+++ Aula05_JS_Pratica_IMC_Pedro_Garrett/script.js
@@ -0,0 +1,33 @@
+// IMC = PESO/(ALTURA*ALTURA)
+
+// - Abaixo de 17 - Muito abaixo do peso
+// - Entre 17 e 18,49 - Abaixo do peso
+// - Entre 18,50 e 24,99 - Peso normal
+// - Entre 25 e 29,99 - Acima do peso
+// - Acima de 30 - Obesidade
+
+var peso;
+var altura;
+var imc;
+var resultado;
+
+//1 recebendo o evento
+function calcular(event){
+        //refresh dos imputs
+
+        event.preventDefault();
+        //alert('Teste');
+        
+        //recebendo os dados digitados pelo usuário
+        peso = document.getElementById('peso').value;
+        altura = document.getElementById('altura').value;
+
+        //calculo do imc
+        imc = peso/(altura*altura);
+
+        //testando a recepção dos valores de peso e altura
+        // testando calculo de imc
+        console.log(peso);
+        console.log(altura);
+        console.log(imc);
+}
