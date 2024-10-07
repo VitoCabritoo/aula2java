2e0bad47ee1ab7c4bcf12d1ea909a11a6b491ca5
midnight2606 <161714635+midnight2606@users.noreply.github.com>
2024-10-07 09:01:57 -0300

    Add files via upload

--- Aula05_JS_Pratica_IMC_Pedro_Garrett/script.js
+++ Aula05_JS_Pratica_IMC_Pedro_Garrett/script.js
@@ -30,4 +30,9 @@ function calcular(event){
         console.log(peso);
         console.log(altura);
         console.log(imc);
+
+        resultado = document.getElementById('resultado');
+        if(imc<17){
+            resultado.innerHTML = '</br> Seu resultado foi: '+imc.toFixed(2)+'</br>Cuidado, você está muito abaixo do seu peso'
+        }
 }
